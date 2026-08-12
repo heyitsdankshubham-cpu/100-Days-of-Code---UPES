7fe2a76c5ec8ebf091bde01217e7e1af175521d8
heyitsdankshubham-cpu <hey.its.dank.shubham@gmail.com>
2026-08-12 15:44:17 +0530

    Add files via upload

---
+++ Q005.c
@@ -0,0 +1,14 @@
+//write a program to convert temprature from celsius to fahrenheit
+#include <stdio.h>
+
+int main() {
+    int a;
+    printf("Enter the temprature in degree celsius: ");
+    scanf("%d",&a);
+
+    printf("Fahrenheit=%d",(a*9/5)+32);
+
+
+    return 0;
+
+}
---
+++ Q006.c
@@ -0,0 +1,12 @@
+//write a program to swap two numbers using a third variable
+#include <stdio.h>
+
+int main() {
+    int a,b;
+    printf("Enter the value of both numbers: ");
+    scanf("%d %d",&a,&b);
+     
+    printf("After swap: %d %d",b,a);
+    return 0;
+
+}
